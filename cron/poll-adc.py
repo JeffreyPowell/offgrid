8e3e900cc2c9e0155ffcd93a233444a9037cbca6
Jeff <Jffrypwll@googlemail.com>
2019-06-25 00:01:10 +0100

    Update poll-adc.py

--- cron/poll-adc.py
+++ cron/poll-adc.py
@@ -77,53 +77,55 @@ print(str(s2))
 #os.system('/usr/bin/rrdtool update /home/pi/offgrid/data/adc-volts-1.rrd '+str(t)+':'+str(s1))
 #os.system('/usr/bin/rrdtool update /home/pi/offgrid/data/adc-volts-2.rrd '+str(t)+':'+str(s2))
 
-sensor_id =1
+sensor_id = 1
+data = +str(s1)
 
 filename = '/home/pi/offgrid/data/s-'+str(sensor_id)+'.rrd'
   
-  print filename
+print (filename)
 
-  if( not os.path.exists( filename ) ):
-      print ( os.path.exists( filename ))
-      os.system('/usr/bin/rrdtool create '+filename+' --step 60 \
-      --start now \
-      DS:data:GAUGE:120:U:U \
-      RRA:MIN:0.5:1:10080 \
-      RRA:MIN:0.5:5:51840 \
-      RRA:MIN:0.5:60:8760 \
-      RRA:AVERAGE:0.5:1:10080 \
-      RRA:AVERAGE:0.5:5:51840 \
-      RRA:AVERAGE:0.5:60:8760 \
-      RRA:MAX:0.5:1:10080 \
-      RRA:MAX:0.5:5:51840 \
-      RRA:MAX:0.5:60:8760')
+if( not os.path.exists( filename ) ):
+        print ( os.path.exists( filename ))
+        os.system('/usr/bin/rrdtool create '+filename+' --step 60 \
+        --start now \
+        DS:data:GAUGE:120:U:U \
+        RRA:MIN:0.5:1:10080 \
+        RRA:MIN:0.5:5:51840 \
+        RRA:MIN:0.5:60:8760 \
+        RRA:AVERAGE:0.5:1:10080 \
+        RRA:AVERAGE:0.5:5:51840 \
+        RRA:AVERAGE:0.5:60:8760 \
+        RRA:MAX:0.5:1:10080 \
+        RRA:MAX:0.5:5:51840 \
+        RRA:MAX:0.5:60:8760')
 
-  if( data != 'NULL' ):
-    print"rrd"
-    os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+str(s1))
+if( data != 'NULL' ):
+        print("rrd")
+        os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+data)
         
-sensor_id =2
+sensor_id = 2
+data = +str(s2)
 
 filename = '/home/pi/offgrid/data/s-'+str(sensor_id)+'.rrd'
   
-  print filename
+print (filename)
 
-  if( not os.path.exists( filename ) ):
-      print ( os.path.exists( filename ))
-      os.system('/usr/bin/rrdtool create '+filename+' --step 60 \
-      --start now \
-      DS:data:GAUGE:120:U:U \
-      RRA:MIN:0.5:1:10080 \
-      RRA:MIN:0.5:5:51840 \
-      RRA:MIN:0.5:60:8760 \
-      RRA:AVERAGE:0.5:1:10080 \
-      RRA:AVERAGE:0.5:5:51840 \
-      RRA:AVERAGE:0.5:60:8760 \
-      RRA:MAX:0.5:1:10080 \
-      RRA:MAX:0.5:5:51840 \
-      RRA:MAX:0.5:60:8760')
+if( not os.path.exists( filename ) ):
+        print ( os.path.exists( filename ))
+        os.system('/usr/bin/rrdtool create '+filename+' --step 60 \
+        --start now \
+        DS:data:GAUGE:120:U:U \
+        RRA:MIN:0.5:1:10080 \
+        RRA:MIN:0.5:5:51840 \
+        RRA:MIN:0.5:60:8760 \
+        RRA:AVERAGE:0.5:1:10080 \
+        RRA:AVERAGE:0.5:5:51840 \
+        RRA:AVERAGE:0.5:60:8760 \
+        RRA:MAX:0.5:1:10080 \
+        RRA:MAX:0.5:5:51840 \
+        RRA:MAX:0.5:60:8760')
 
-  if( data != 'NULL' ):
-    print"rrd"
-    os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+str(s2))
+if( data != 'NULL' ):
+        print("rrd")
+        os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+data)
  
