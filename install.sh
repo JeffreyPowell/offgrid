d0292e35cbed8ad368b2e62f676018ce8a23cfb1
jeff <jffrypwll@googlemail.com>
2023-07-22 21:06:57 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -218,7 +218,9 @@ then
     echo $INSTALL_DIR"/offgrid.zip file not found !"
     exit()
 
-	unzip $INSTALL_DIR"/offgrid.zip" 
+	unzip $($INSTALL_DIR"/offgrid.zip") 
+  
+  exit()
   
 	rm -rf $INSTALL_DIR"/offgrid.zip"
   
